8ff948cf9905f408e48ebcd49c2a273a4a5719de
S MOHAMMAD ASHRAF <ashrafali31033@gmail.com>
2024-12-05 16:19:37 +0530

    Add files via upload

---
+++ loop6.java
@@ -0,0 +1,14 @@
+public class loop6 {
+
+    public static void main(String[] args) {
+        int n = 10;
+        int i =n-1;
+        while(i>=0)
+        {
+            System.out.println(n-i);
+            i-=1;
+
+        }
+    }
+    
+}
