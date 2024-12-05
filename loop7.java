b0413d1b2d9161553aa30777109735d7c7406c57
S MOHAMMAD ASHRAF <ashrafali31033@gmail.com>
2024-12-05 16:21:55 +0530

    Add files via upload

---
+++ loop7.java
@@ -0,0 +1,16 @@
+public class loop7 {
+
+    public static void main(String[] args) {
+
+        //printing reverse of numbers
+        int n = 100;
+        int i =n;
+        while(i>-1)
+        {
+            System.out.println(i);
+            i-=1;
+
+        }
+    }
+    
+}
