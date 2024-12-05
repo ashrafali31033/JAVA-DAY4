13871cc0deb7ebfc2aad786233df990eb33dba51
S MOHAMMAD ASHRAF <ashrafali31033@gmail.com>
2024-12-05 16:19:25 +0530

    Add files via upload

---
+++ loop5.java
@@ -0,0 +1,17 @@
+public class loop5 {
+    public static void main(String args[])
+    {
+
+        int n= 10;
+        
+
+        for(int i=1;i<=10;i++)
+        {
+
+            System.out.println(n+"x"+i+"="+n*i);
+
+        }
+        
+    }
+    
+}
